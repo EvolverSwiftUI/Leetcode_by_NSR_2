3a2207fc494fef8c63646bbdefe1f36fce5e1eb1
Sivaram Yadav <nsr.learnings@gmail.com>
2023-08-24 14:14:35 +0530

    435_Non-overlappingIntervals

--- Leetcode-By-NSR-2.playground/Pages/435_Non-overlappingIntervals.xcplaygroundpage/Contents.swift
+++ Leetcode-By-NSR-2.playground/Pages/435_Non-overlappingIntervals.xcplaygroundpage/Contents.swift
@@ -1,2 +1,23 @@
 import Foundation
 
+func eraseOverlapIntervals(_ intervals: [[Int]]) -> Int {
+    if intervals.isEmpty { return 0 }
+    
+    var sorted = intervals.sorted { $0[1] < $1[1] }
+    var count = 0
+    var currentMin = Int.min
+    
+    for i in 0..<sorted.count {
+        if sorted[i][0] >= currentMin {
+            currentMin = sorted[i][1]
+        } else {
+            count += 1
+        }
+    }
+    
+    return count
+}
+
+eraseOverlapIntervals([[1,2],[2,3],[3,4],[1,3]]) // 1
+eraseOverlapIntervals([[1,2],[1,2],[1,2]]) // 2
+eraseOverlapIntervals([[1,2],[2,3]]) // 0
