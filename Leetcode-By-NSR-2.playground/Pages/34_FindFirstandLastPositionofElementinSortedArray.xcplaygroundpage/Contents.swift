39c74cffd431fd036ad6470bed89bead52128a45
Sivaram Yadav <nsr.learnings@gmail.com>
2023-09-05 21:00:06 +0530

    added changes

--- Leetcode-By-NSR-2.playground/Pages/34_FindFirstandLastPositionofElementinSortedArray.xcplaygroundpage/Contents.swift
+++ Leetcode-By-NSR-2.playground/Pages/34_FindFirstandLastPositionofElementinSortedArray.xcplaygroundpage/Contents.swift
@@ -3,21 +3,18 @@ import Foundation
 func searchRange(_ nums: [Int], _ target: Int) -> [Int] {
     if nums.isEmpty { return [-1, -1] }
 
-    var result: [Int] = []
+    var result: [Int] = [-1, -1]
     
     for (index,num) in nums.enumerated() {
         if num == target {
-            result.append(index)
+            if result[0] == -1 { // first time, so fill at first index
+                result[0] = index 
+            }
+            result[1] = index // anytime fill at last index
         }
     }
-    
-    if result.isEmpty {
-        return [-1, -1]
-    } else if result.count == 1 {
-        return [result.first!, result.first!]
-    }
 
-    return [result.first!, result.last!]
+    return result
 }
 
 searchRange([5,7,7,8,8,10], 8) //[3,4]
