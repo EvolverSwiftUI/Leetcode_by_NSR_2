478b04e602a7f43cd867187b9a0600fe8341aff2
Sivaram Yadav <nsr.learnings@gmail.com>
2023-09-13 23:22:38 +0530

    162_FindPeakElement_optimal_approach

--- Leetcode-By-NSR-2.playground/Pages/162_FindPeakElement.xcplaygroundpage/Contents.swift
+++ Leetcode-By-NSR-2.playground/Pages/162_FindPeakElement.xcplaygroundpage/Contents.swift
@@ -20,6 +20,31 @@ findPeakElement([1,2,1,3,5,6,4]) //  1 or 5
 
 // optimal approach:
 func findPeakElement_v2(_ nums: [Int]) -> Int {
+    let n = nums.count
+    
+    if n == 1 { return 0 } // exception case
+    if nums[0] > nums[1] { return 0 } // edge case 1
+    if nums[n-1] > nums[n-2] { return n-1 } // edge case 2
+
+    var left = 1
+    var right = n-2
+    
+    while left <= right {
+        let mid = (left+right)/2
+        if (nums[mid] > nums[mid-1]) && (nums[mid] > nums[mid+1]) {
+            return mid
+        }
+        else if nums[mid] > nums[mid-1] {
+            left = mid+1
+        }
+        else if nums[mid] > nums[mid+1] {
+            right = mid-1
+        }
+        else {
+            left = mid+1
+        }
+    }
+    
     return -1
 }
 
